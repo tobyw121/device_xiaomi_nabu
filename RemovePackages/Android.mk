4f3e5482b113a8b0433dcb762ed5bf122da8eda2
Toby <tobiasw763@gmail.com>
2024-11-17 17:27:15 +0100

    Add files via upload

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+   CarrierServices \
+   Dialer \
+   GoogleDialer \
+   Stk \
+   TeleService \
+   Telecom \
+   GoogleContacts \
+   com.google.android.dialer.support \
+   com.android.phone.common \
+   DevicePersonalizationPrebuiltPixel2020 \
+   messaging
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
